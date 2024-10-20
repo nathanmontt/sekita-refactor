fa6929534ba9ad5e2692e7f261d07259f6e3b51e
nathanmontt <contato.mont@gmail.com>
2024-10-20 13:55:01 -0300

    Mudança no estilo. Adição e modificação da index.js Modificação no arquivo-base, indez index.html

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// // Pegar o icon do menu
+// const openMenuIcon = document.getElementById("open-menu");
+
+// // Pegar o elemento div que contém as opções
+// const closeMenuIcon = document.getElementById("close-menu");
+
+// openMenuIcon.addEventListener('click', () => {
+//     console.log("hello world!");
+// });
